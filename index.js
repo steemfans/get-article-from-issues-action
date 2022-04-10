5a77d90b8ae47a3634de63d2714c7182d12b1bff
ety001 <work@akawa.ink>
2022-04-10 01:50:48 +0000

    first version

---
+++ index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function main() {
+  const token = core.getInput('token');
+  const repo = core.getInput('repo');
+  const owner = core.getInput('owner');
+
+  const octokit = github.getOctokit(token);
+  const issueList = await octokit.rest.issues.listForRepo({
+    owner,
+    repo,
+    state: 'open',
+    direction: 'asc',
+    per_page: 10,
+    page: 1,
+  });
+
+  console.log('issueList:', issueList);
+
+  // You can also pass in additional options as a second parameter to getOctokit
+  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+}
+
+main();
